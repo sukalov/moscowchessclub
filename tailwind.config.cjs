237391d27ac242d2be6d2a045abcb7e523851d6c
sukalov <matvey10@gmail.com>
2023-09-12 16:07:02 +0300

    photobook getting pic from googledrive test

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,53 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	darkMode: ["class"],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				border: "var(--border)",
+				input: "var(--input)",
+				ring: "var(--ring)",
+				background: "var(--background)",
+				foreground: "var(--foreground)",
+				primary: {
+					DEFAULT: "var(--primary)",
+					foreground: "var(--primary-foreground)",
+					},
+				secondary: {
+					DEFAULT: "var(--secondary)",
+					foreground: "var(--secondary-foreground)",
+					},
+				destructive: {
+					DEFAULT: "var(--destructive)",
+					foreground: "var(--destructive-foreground)",
+					},
+				muted: {
+					DEFAULT: "var(--muted)",
+					foreground: "var(--muted-foreground)",
+					},
+				accent: {
+					DEFAULT: "var(--accent)",
+					foreground: "var(--accent-foreground)",
+					},
+				popover: {
+					DEFAULT: "var(--popover)",
+					foreground: "var(--popover-foreground)",
+					},
+				card: {
+					DEFAULT: "var(--card)",
+					foreground: "var(--card-foreground)",
+					},
+				mcc: {
+					red: "var(--mcc-red)",
+					green: "var(--mcc-green)",
+					yellow: "var(--mcc-yellow)",
+					darkyellow: "var(--mcc-darkyellow)",
+					black: "var(--mcc-black)",
+
+				}
+			},
+		},
 	},
 	plugins: [],
 }
